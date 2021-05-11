712c90009b41816d9c043bbf6efa7e016f5353bf
Yva <yvahajatiana@gmail.com>
2021-05-11 18:02:12 +0200

    feat(Upsell): add Upseel domain

---
+++ Core/Upsell/Contracts/IUpsellRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Shared.Contracts;
+using Core.Upsell.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Upsell.Contracts
+{
+    public interface IUpsellRepository : IRepositoryBase<UpsellEntity>
+    {
+        Task<IEnumerable<UpsellEntity>> GetUpsells();
+
+        Task<UpsellEntity> GetUpsellById(string id);
+    }
+}
