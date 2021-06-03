aa669ef29e3df7265f700ce0097caac8086a0b32
Yva <yvahajatiana@gmail.com>
2021-06-03 05:49:55 +0200

    [FRONT] - feat(Upsell): Implement CRUD btn

--- Shopiall/ClientApp/src/app/upsell/components/upsell-list/upsell-list.component.ts
+++ Shopiall/ClientApp/src/app/upsell/components/upsell-list/upsell-list.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { isNullOrUndefined } from 'src/app/shared/helpers/instance.helpers';
 import { selectUpsellList } from '../../store';
-import { loadUpsellList } from '../../store/upsell.actions';
+import { deleteUpsell, loadUpsellList } from '../../store/upsell.actions';
 import { colDefs, Upsell } from '../../upsell.model';
 
 @Component({
@@ -11,9 +13,16 @@ import { colDefs, Upsell } from '../../upsell.model';
   styleUrls: ['./upsell-list.component.css'],
 })
 export class UpsellListComponent implements OnInit {
+  private gridApi;
+  private gridColumnApi;
   upsellList$: Observable<Upsell[]>;
   columnDefs = colDefs;
-  constructor(private store: Store) {}
+  selectedUpsell: Upsell;
+  constructor(
+    private store: Store,
+    private roter: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.tableInit();
@@ -22,5 +31,45 @@ export class UpsellListComponent implements OnInit {
   tableInit(): void {
     this.store.dispatch(loadUpsellList());
     this.upsellList$ = this.store.pipe(select(selectUpsellList));
+  }
+
+  create(): void {
+    this.roter.navigateByUrl('create');
+  }
+
+  edit(): void {
+    this.roter.navigate(['edit', this.selectedUpsell.id], {
+      relativeTo: this.route,
+    });
+  }
+
+  remove(): void {
+    if (isNullOrUndefined(this.selectedUpsell)) {
+      return;
+    }
+    if (!confirm(`Are you sure to delete: ${this.selectedUpsell.title}`)) {
+      return;
+    }
+    this.store.dispatch(deleteUpsell({ id: this.selectedUpsell.id }));
+    this.store.dispatch(loadUpsellList());
+    this.reset();
+  }
+
+  onSelectionChanged(): void {
+    const selectedRows: Upsell[] = this.gridApi.getSelectedRows();
+    if (isNullOrUndefined(selectedRows)) {
+      return;
+    }
+    this.selectedUpsell = selectedRows[0];
+    console.log(this.selectedUpsell);
+  }
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+  }
+
+  reset(): void {
+    this.selectedUpsell = undefined;
   }
 }
